1ad56361f27acacbd931e9d06a02fa0740999b7d
ktucalvin <ktucalvin@gmail.com>
2018-12-30 15:22:05 -0700

    Add ability to echo to different channels

--- commands/echo.js
+++ commands/echo.js
@@ -1,13 +1,43 @@
 'use strict'
+const i18n = require('$lib/i18n')
+const { getopts } = require('$lib/parser')
+const optmap = new Map()
+  .set('to', { alias: 't', hasParam: true })
+  .set('setref', { alias: 's', hasParam: true })
+  .set('getref', { alias: 'g' })
+let ref
+
 module.exports = {
   name: 'echo',
+  optmap,
   aliases: ['say'],
   usage: ['echo [string]'],
+  permission: 'MANAGE_GUILD',
   execute (message, args) {
     if (!args.length) {
       message.channel.send('_ _') // send blank line
       return
     }
-    message.channel.send(args.join(' '))
+
+    const { __, _s } = i18n.useGuild(message.guild.id)
+    const opts = getopts(args, optmap)
+    if (opts.get('flags').length) {
+      ref ? message.channel.send(_s('echo.getref.notice', `<#${ref.id}>`)) : message.channel.send(__('echo.getref.noReference'))
+      return
+    }
+
+    const setref = opts.get('setref')
+    const out = opts.get('to') ? message.mentions.channels.find(cmd => opts.get('to').includes(cmd.id)) : undefined
+    ref = setref ? message.mentions.channels.find(cmd => setref.includes(cmd.id)) : ref
+    if (setref === 'none') { ref = null; message.channel.send(__('echo.clearRef')) }
+
+    const str = args.join(' ') + '_ _'
+    if (out) {
+      out.send(str)
+    } else if (ref) {
+      str !== '_ _' ? ref.send(str) : message.channel.send(_s('echo.setref.notice', setref))
+    } else {
+      message.channel.send(str)
+    }
   }
 }
--- commands/test/echo.test.js
+++ commands/test/echo.test.js
@@ -3,31 +3,92 @@
 require('module-alias/register')
 const chai = require('chai')
 const sinon = require('sinon')
+const i18n = require('$lib/i18n')
 const Message = require('$structures/FakeMessage')
-const echo = require('../echo').execute
+let echo = require('../echo').execute
 const expect = chai.expect
+chai.use(require('dirty-chai'))
 chai.use(require('sinon-chai'))
 
 describe('echo', function () {
-  let spy, message
+  let originalChannel, message, zeroChannel, translate
   beforeEach(function () {
+    delete require.cache[require.resolve('../echo')]
+    echo = require('../echo').execute
     message = new Message()
-    spy = sinon.spy(message.channel, 'send')
+    const channel = { id: '0', send: () => {} }
+    message.mentions.channels.set('0', channel)
+    originalChannel = sinon.spy(message.channel, 'send')
+    zeroChannel = sinon.spy(channel, 'send')
+    translate = sinon.spy(i18n, 'translate')
   })
-  afterEach(function () { spy.restore() })
+  afterEach(function () { translate.restore() })
 
   it('echoes blank line given no args', function () {
     echo(message, [])
-    expect(spy).to.be.calledWith('_ _')
+    expect(originalChannel).to.be.calledWith('_ _')
   })
 
   it('echoes a given string', function () {
     echo(message, ['a', 'given', 'string'])
-    expect(spy).to.be.calledWith('a given string')
+    expect(originalChannel).to.be.calledWith('a given string_ _')
   })
 
-  it('preserves whitespace', function () {
-    echo(message, ['s p a c e d', ' string '])
-    expect(spy).to.be.calledWith('s p a c e d  string ')
+  describe('#to', function () {
+    it('prints to the specified channel', function () {
+      echo(message, ['--to', '<#0>', 'string'])
+      expect(zeroChannel).to.be.calledWith('string_ _')
+    })
+
+    it('prefers --to over --setref if both are given', function () {
+      const to = { id: '1', send: () => {} }
+      const toSpy = sinon.spy(to, 'send')
+      message.mentions.channels.set('1', to)
+      echo(message, ['--setref', '<#0>', 'string', '--to', '<#1>'])
+      expect(zeroChannel).to.not.be.called()
+      expect(toSpy).to.be.calledWith('string_ _')
+    })
+  })
+
+  describe('#setref', function () {
+    it('prints to the specified channel', function () {
+      echo(message, ['--setref', '<#0>', 'string'])
+      expect(zeroChannel).to.be.calledWith('string_ _')
+    })
+
+    it('continues to print to the reference channel', function () {
+      echo(message, ['--setref', '<#0>', 'the first call'])
+      echo(message, ['--setref', '<#0>', 'the second call'])
+      expect(zeroChannel).to.be.calledTwice()
+        .and.to.be.calledWith('the first call_ _')
+        .and.to.be.calledWith('the second call_ _')
+    })
+
+    it('clears the reference channel when given "none"', function () {
+      echo(message, ['--setref', 'none', 'string'])
+      echo(message, ['second'])
+      expect(translate).to.be.calledWith('echo.clearRef')
+      expect(originalChannel).to.be.calledWith('string_ _')
+        .and.to.be.calledWith('second_ _')
+    })
+
+    it('only sends to reference channel if there is a message', function () {
+      echo(message, ['--setref', '<#0>'])
+      expect(zeroChannel).to.not.be.called()
+      expect(translate).to.be.calledWith('echo.setref.notice')
+    })
+  })
+
+  describe('#getref', function () {
+    it('notifies user if no reference channel is set', function () {
+      echo(message, ['--getref'])
+      expect(translate).to.be.calledWith('echo.getref.noReference')
+    })
+
+    it('prints the reference channel when it is set', function () {
+      echo(message, ['--setref', '<#0>'])
+      echo(message, ['--getref'])
+      expect(translate).to.be.calledWith('echo.getref.notice')
+    })
   })
 })
