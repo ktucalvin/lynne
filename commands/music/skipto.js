de97a869dcd1e105c940a0e26d7972136e726529
ktucalvin <ktucalvin@gmail.com>
2018-12-31 23:15:38 -0700

    Add skipto command

---
+++ commands/music/skipto.js
@@ -0,0 +1,34 @@
+'use strict'
+const i18n = require('$lib/i18n')
+const manager = require('./QueueManager')
+module.exports = {
+  name: 'skipto',
+  usage: ['skipto index'],
+  execute (message, args) {
+    const { __ } = i18n.useGuild(message.guild.id)
+    const dispatcher = manager.getDispatcher(message.guild.id)
+    const Q = manager.getQueue(message.guild.id)
+    const index = parseInt(args[0])
+
+    if (!Q || !Q.length) {
+      message.channel.send(__('queue.notPlaying'))
+      return
+    }
+    if (!args.length || !(typeof index === 'number' && index % 1 === 0)) {
+      message.channel.send(__('skipto.invalidIndex'))
+      return
+    }
+    if (index > Q.length || index < 1) {
+      message.channel.send(__('skipto.indexOutOfBounds'))
+      return
+    }
+    if (index === 1) {
+      message.channel.send(__('skipto.cannotSkipCurrentSong'))
+      return
+    }
+
+    // note dispatcher.end will cause the queue to shift one more time
+    Q.splice(0, index - 2)
+    dispatcher.end()
+  }
+}
